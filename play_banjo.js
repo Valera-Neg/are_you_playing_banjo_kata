c1cba1717efa375bea4862470a8572ed4274bd59
Valera-Neg <nehovora.val@gmail.com>
2022-08-06 10:22:48 -0400

    first commit

---
+++ play_banjo.js
@@ -0,0 +1,10 @@
+function areYouPlayingBanjo(name) {
+  return name[0] === "r" || name[0] === "R" ? `${name} plays banjo` : `${name} does not play banjo`
+
+}
+
+console.log(areYouPlayingBanjo("Adam")) //> "Adam does not play banjo"
+console.log(areYouPlayingBanjo("Paul")) //> "Paul does not play banjo"
+console.log(areYouPlayingBanjo("Ringo")) //> "Ringo plays banjo"
+console.log(areYouPlayingBanjo("bravo")) //> "bravo does not play banjo"
+console.log(areYouPlayingBanjo("rolf")) //> "rolf plays banjo"
